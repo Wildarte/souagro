ec36810ac15eec5f054cb0d8d1a2b24b07aff248
wildarte <wildarte10@gmail.com>
2022-09-07 13:38:45 -0300

    opção de search, submenus dropdowns e dropdowns

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,46 @@ $(document).ready(function(){
     })
 
 });
+
+const btn_open_menu = document.querySelector('.btn_menu');
+const btn_close_menu = document.querySelector('.btn_close_menu');
+const menu = document.querySelector('nav.m-menu');
+
+btn_open_menu.addEventListener('click', () => {
+
+    menu.classList.add('open_menu');
+
+});
+
+btn_close_menu.addEventListener('click', () => {
+
+    menu.classList.remove('open_menu');
+
+});
+
+
+const open_search = document.getElementById('open_search');
+const search_over = document.querySelector('.search_over');
+const btn_slose_search = document.getElementById('btn_slose_search');
+
+open_search.addEventListener('click', () => {
+
+    search_over.classList.add('open_search');
+
+});
+
+btn_slose_search.addEventListener('click', () => {
+
+    search_over.classList.remove('open_search');
+
+});
+
+const item_has_children = document.querySelectorAll('li.menu-item-has-children > a');
+
+item_has_children.forEach((item) => {
+
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+    });
+
+})
