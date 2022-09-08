c629dcbe3eb3772d0498809bfff6f8e6c818bd82
wildarte <wildarte10@gmail.com>
2022-09-08 12:42:17 -0300

    criacao da pagina de noticia

--- noticia.php
+++ noticia.php
@@ -2,9 +2,300 @@
     include('./header.php');
 ?>
 
-<main>
+    <div class="container m-container">
+        <div class="ga"></div>
+    </div>
+
+    <main>
+
+        <section class="container m-container">
+
+            <div class="row">
+
+                <div class="col-12">
+
+                    <h2 class="title_post">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, consequuntur?</h2>
+
+                </div>
+
+            </div>
+
+
+            <div class="info_post col-12 col-xl-8 p-0">
+
+                <div class="author_post">
+
+                        <img class="author_thumb" src="https://cdn.pixabay.com/photo/2017/02/16/23/10/smile-2072907_960_720.jpg" alt="">
+
+                        <div>
+                            <h3>Débora Damasceno</h3>
+
+                            <time>
+                                01 de agosto de 2022 às 21:29
+                            </time>
+
+                        </div>
+                        
+                </div>
+
+                <div class="links_share">
+
+                    <a class="link_share_post" href="">
+                        <i class="bi bi-facebook"></i>
+                    </a>
+
+                    <a class="link_share_post" href="">
+                        <i class="bi bi-twitter"></i>
+                    </a>
+
+                    <a class="link_share_post" href="">
+                        <i class="bi bi-whatsapp"></i>
+                    </a>
+
+                    <a class="link_share_post" href="">
+                        <i class="bi bi-telegram"></i>
+                    </a>
+
+                </div>
+
+            </div>
+            
+
+            <div class="row">
+
+                <div class="col-12 col-xl-8">
+
+                    <figure>
+                        <img class="img-response-header img_post" src="https://cdn.pixabay.com/photo/2016/11/17/14/24/soy-1831703_960_720.jpg" alt="" style="">
+                        <figcaption>Foto: Lorem ipsum dolor sit.</figcaption>
+                    </figure>
+
+                    <!-- content post -->
+                    <div class="content_post">
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, eveniet dolorum, libero autem eligendi possimus facilis expedita enim aut itaque, nulla ipsum. Perferendis, ad doloremque veritatis voluptatum asperiores sequi labore ipsa optio neque. Obcaecati enim non, suscipit mollitia facere distinctio molestiae esse commodi sed voluptate deserunt, dolorum rerum ullam id.</p>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis iste in quia cupiditate, excepturi atque minus? Impedit expedita doloribus tempore officia ea molestiae, delectus a dolore, ex facilis minus veritatis deserunt! Reiciendis velit totam possimus iste vero laudantium, illo culpa facilis inventore aspernatur ab dolor corporis saepe sint aut architecto incidunt quaerat sapiente nemo id sequi laboriosam? Exercitationem, ab libero.</p>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vero deleniti sit incidunt laborum atque maiores, molestiae eos eveniet labore commodi dolorum obcaecati! Assumenda, odio incidunt! Non possimus ipsam modi, necessitatibus nam accusamus quasi perspiciatis repellat eum commodi minima libero, reprehenderit quidem. Architecto, pariatur quibusdam!</p>
+
+                        <div class="square_ads" style="max-width: 380px; margin: 30px auto">
+                            <img src="assets/img/ads_square.png" alt="">
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, impedit! Doloribus placeat praesentium excepturi quos. Numquam distinctio saepe cumque quae vero libero ea eligendi nostrum quas dolores inventore repellat suscipit officiis earum fugit repellendus hic, magnam ad nisi eius voluptatum! Molestiae placeat illum ullam nihil architecto magni, minima sequi nemo neque dolore reprehenderit amet nulla.</p>
+
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit vel repudiandae ea quod iure ipsam quae dolore doloremque earum ullam repellat, nulla voluptates ab eaque facilis quis sapiente veritatis. Libero, aut voluptatum! Illum possimus excepturi saepe doloribus similique incidunt iure, molestias at odio.</p>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perspiciatis placeat assumenda harum repudiandae laboriosam illo velit eos vel itaque, ducimus praesentium alias doloribus mollitia perferendis tempore quaerat at culpa!</p>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti aspernatur hic aliquid nihil asperiores nostrum culpa itaque debitis dolore sequi porro ex id reiciendis eligendi, at labore saepe! Perspiciatis distinctio minima totam nihil voluptas beatae delectus dolorum obcaecati consequatur nesciunt corrupti, neque debitis corporis dolores necessitatibus repellendus? Sit, perspiciatis cum.
+                        </p>
+
+
+                    </div>
+                    <!-- content post -->
+
+                    <div class="info_post col-12 p-0">
+
+                        <div class="author_post">
+
+                                <img class="author_thumb" src="https://cdn.pixabay.com/photo/2017/02/16/23/10/smile-2072907_960_720.jpg" alt="">
+
+                                <div>
+                                    <h3>Débora Damasceno</h3>
+
+                                    <time>
+                                        01 de agosto de 2022 às 21:29
+                                    </time>
+
+                                </div>
+                                
+                        </div>
+
+                        <div class="links_share">
+
+                            <a class="link_share_post" href="">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+
+                            <a class="link_share_post" href="">
+                                <i class="bi bi-twitter"></i>
+                            </a>
+
+                            <a class="link_share_post" href="">
+                                <i class="bi bi-whatsapp"></i>
+                            </a>
+
+                            <a class="link_share_post" href="">
+                                <i class="bi bi-telegram"></i>
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-12 col-xl-4">
+
+                    <div class="square_ads">
+                        <img src="assets/img/ads_square.png" alt="">
+                    </div>
+
+                    <div class="cta_short p-40-0">
+                        <div class="header_cta_short">
+                            <h3 class="title_cta_social color-dark">
+                                Entre em um<br> dos grupos!
+                            </h3>
+                        </div>
+                        <div class="col-12 col-xl-6 social_links p-0">
+                            <a href="" class="btn_social bg-green">
+                                whatsapp
+                            </a>
+                            <a href="" class="btn_social bg-blue">
+                                Telegram
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="square_ads">
+                        <img src="assets/img/ads_square.png" alt="">
+                    </div>
+
+                    <div class="row">
+                        <h2 class="title_green text-left m-0 title_more_read p-10">Mais Lidas</h2>
+                    </div>
     
-</main>
+                    <section class=" list_more">
+                        <article class="card_small">
+                            <a href="">
+                                <h3 class="title_card_thin"><span class="count_item"></span> Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+                            </a>
+                        </article>
+    
+                        <article class="card_small">
+                            <a href="">
+                                <h3 class="title_card_thin"><span class="count_item"></span> Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+                            </a>
+                        </article>
+    
+                        <article class="card_small">
+                            <a href="">
+                                <h3 class="title_card_thin"><span class="count_item"></span> Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+                            </a>
+                        </article>
+    
+                        <article class="card_small">
+                            <a href="">
+                                <h3 class="title_card_thin"><span class="count_item"></span> Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+                            </a>
+                        </article>
+
+                        <article class="card_small">
+                            <a href="">
+                                <h3 class="title_card_thin"><span class="count_item"></span> Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+                            </a>
+                        </article>
+
+                    </section>
+
+                    <div class="ads_vert">
+                        <img src="assets/img/ads_vert.png" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </section>
+
+        <div class="container m-container">
+            <div class="ga"></div>
+
+        </div>
+        
+        <section class="container m-container">
+
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="title_green">Notícias Relacionadas</h2>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <article class="card_default">
+                        <a href="">
+
+                            <img src="https://cdn.pixabay.com/photo/2015/02/18/16/10/wheat-640960_960_720.jpg" alt="">
+                            
+                            <div class="card_default_info">
+                                <h3 class="subtitle">Editora</h3>
+                            
+                                <h2 class="title_card">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    
+                                <p class="desc_card">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur omnis tempore necessitatibus iste eum optio quis fugit</p>
+    
+                                <span class="btn_green">Ler Mais</span>
+                            </div>
+
+                        </a>
+                        
+                    </article>
+                </div>
+                <div class="col-12 col-md-4">
+                    <article class="card_default">
+                        <a href="">
+
+                            <img src="https://cdn.pixabay.com/photo/2015/02/18/16/10/wheat-640960_960_720.jpg" alt="">
+                            
+                            <div class="card_default_info">
+                                <h3 class="subtitle">Editora</h3>
+                            
+                                <h2 class="title_card">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    
+                                <p class="desc_card">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur omnis tempore necessitatibus iste eum optio quis fugit</p>
+    
+                                <span class="btn_green">Ler Mais</span>
+                            </div>
+
+                        </a>
+                        
+                    </article>
+                </div>
+                <div class="col-12 col-md-4">
+                    <article class="card_default">
+                        <a href="">
+
+                            <img src="https://cdn.pixabay.com/photo/2015/02/18/16/10/wheat-640960_960_720.jpg" alt="">
+                            
+                            <div class="card_default_info">
+                                <h3 class="subtitle">Editora</h3>
+                            
+                                <h2 class="title_card">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    
+                                <p class="desc_card">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur omnis tempore necessitatibus iste eum optio quis fugit</p>
+    
+                                <span class="btn_green">Ler Mais</span>
+                            </div>
+
+                        </a>
+                        
+                    </article>
+                </div>
+            </div>
+
+        </section>
+
+        <div class="container m-container">
+            <div class="ga"></div>
+
+        </div>
+        
+    </main>
 
 <?php
     include('./footer.php');
